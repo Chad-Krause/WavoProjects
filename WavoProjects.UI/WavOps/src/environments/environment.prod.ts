c086e68c39594614820020b6ee59eb78aefcbf85
Chad-Krause <38547226+Chad-Krause@users.noreply.github.com>
2026-01-02 11:44:43 -0500

    Add container build for full stack and serve static UI

--- WavoProjects.UI/WavOps/src/environments/environment.prod.ts
+++ WavoProjects.UI/WavOps/src/environments/environment.prod.ts
@@ -1,6 +1,10 @@
-export const environment = {
+import { buildEnvironment } from './environment.util';
+
+const defaults = {
   production: true,
   dragUpdateDelayMS: 33,
-  baseUrl: "https://apiv2.waverlyrobotics.org/",
-  hubUrl: "https://apiv2.waverlyrobotics.org/WavOpsHub"
+  baseUrl: 'https://apiv2.waverlyrobotics.org/',
+  hubUrl: 'https://apiv2.waverlyrobotics.org/WavOpsHub',
 };
+
+export const environment = buildEnvironment(defaults);
